5785d8aee9e8024bf809bc62d7da588750f833cd
miqla <miqdad.shaldin@gmail.com>
2021-10-20 19:44:06 +0700

    ngerubah Function Declaration, nambahin Object.create didalamnya

--- script.js
+++ script.js
@@ -2,45 +2,58 @@
 
 // 1. Object Literal
 // PROBLEM : tidak efektif untuk object yg banyak
-let mahasiswa = {
-  // properti, isinya adalah nilai / value
-  nama: 'Mila',
-  energi: 10,
-  // method, adalah function didalam object, method isinya function
-  makan: function (porsi) {
-    this.energi = this.energi + porsi;
-    console.log(`Halo ${this.nama}, selamat makan!`);
-  }
-}
+// let mahasiswa = {
+//   // properti, isinya adalah nilai / value
+//   nama: 'Mila',
+//   energi: 10,
+//   // method, adalah function didalam object, method isinya function
+//   makan: function (porsi) {
+//     this.energi = this.energi + porsi;
+//     console.log(`Halo ${this.nama}, selamat makan!`);
+//   }
+// }
 
-// kalo bikin 2, nama variabelnya gabole sama
-let mahasiswa2 = {
-  nama: 'Milar',
-  energi: 10,
-  makan: function (porsi) {
-    this.energi = this.energi + porsi;
-    console.log(`Halo ${this.nama}, selamat makan!`);
-  }
-}
+// // kalo bikin 2, nama variabelnya gabole sama
+// let mahasiswa2 = {
+//   nama: 'Milar',
+//   energi: 10,
+//   makan: function (porsi) {
+//     this.energi = this.energi + porsi;
+//     console.log(`Halo ${this.nama}, selamat makan!`);
+//   }
+// }
 
-
+// ---------------------/----------------/------------------/---------------
 
 // 2. Function Declaration
-    // templatenya
-function Mahasiswa(nama, energi) {
-  let mahasiswa = {};
-  mahasiswa.nama = nama;
-  mahasiswa.energi = energi;
-
-  mahasiswa.makan = function (porsi) {
+// biar function yg ga dibutuhin ga dijalankan, kita buat object yg isinya (2 buah) method
+const methodMahasiswa = {
+  makan : function (porsi) {
     this.energi += porsi;
     console.log(`Halo ${this.nama}, selamat makan!`);
-  }
+  },
 
-  mahasiswa.main = function (jam) {
+  main : function (jam) {
     this.energi -= jam;
     console.log(`Halo ${this.nama}, selamat bermain!`);
-  }
+    },
+
+  tidur : function (jam) {
+    this.energi += jam * 2;
+    console.log(`Halo ${this.nama}, selamat tidur!`);
+    }
+  };
+
+    // templatenya
+function Mahasiswa(nama, energi) {
+  // let mahasiswa = {};
+  // Object.create(methodMahasiswa): ikut membawa properti dan method dari object lain, parent nya
+  let mahasiswa = Object.create(methodMahasiswa);
+  mahasiswa.nama = nama;
+  mahasiswa.energi = energi;
+  mahasiswa.makan = methodMahasiswa.makan;
+  mahasiswa.main = methodMahasiswa.main;
+  mahasiswa.tidur = methodMahasiswa.tidur;
 
   // kalo function declaration harus ada return, else null
   return mahasiswa;
@@ -50,30 +63,33 @@ function Mahasiswa(nama, energi) {
 let mila = Mahasiswa('Mila', 10);
 let erik = Mahasiswa('Erik', 15);
 
-
+// ---------------------/----------------/------------------/---------------
 
 
 // 3. Constructor Function
 // keyword new
-function Mahasiswa(nama, energi) {
-  // let mahasiswa = {};      gaperlu nulisin variabelnya
-  this.nama = nama;
-  this.energi = energi;
+// function Mahasiswa(nama, energi) {
+//   // let mahasiswa = {};      gaperlu nulisin variabelnya
+//   this.nama = nama;
+//   this.energi = energi;
 
-  this.makan = function (porsi) {
-    this.energi += porsi;
-    console.log(`Halo ${this.nama}, selamat makan!`);
-  }
+//   this.makan = function (porsi) {
+//     this.energi += porsi;
+//     console.log(`Halo ${this.nama}, selamat makan!`);
+//   }
 
-  this.main = function (jam) {
-    this.energi -= jam;
-    console.log(`Halo ${this.nama}, selamat bermain!`);
-  }
-  // return mahasiswa;    gaperlu return
-}
+//   this.main = function (jam) {
+//     this.energi -= jam;
+//     console.log(`Halo ${this.nama}, selamat bermain!`);
+//   }
+//   // return mahasiswa;    gaperlu return
+// }
 
-// jangan lupa pake new
-let lorem = new Mahasiswa('Lorem', 12);
+// // jangan lupa pake new
+// let lorem = new Mahasiswa('Lorem', 12);
+
+
+// ---------------------/----------------/------------------/---------------
 
 
 // 4. Object.create
