ce8bae85c5af295742127adb3b9adf100317845b
miqla <miqdad.shaldin@gmail.com>
2021-10-21 13:12:04 +0700

    Arrow Function lanjutan, memahami konsep this pada arrow func, contoh implementasi pada real web, animation

--- arrowfunc.js
+++ arrowfunc.js
@@ -32,9 +32,10 @@
 
 // -----------------/-------------------/------------------/-------------------/-----------
 
+
 // function mapping
 // menghitung jumlah huruf dari masing" mahasiswa, lalu each jumlah dijadikan array
-let mahasiswa = ['Mila Rabbani', 'Lorem Ipsum', 'Ipsum'];
+// let mahasiswa = ['Mila Rabbani', 'Lorem Ipsum', 'Ipsum'];
 
 // cara biasa
 // let jumlahHuruf = mahasiswa.map(function(nama) {
@@ -47,5 +48,85 @@ let mahasiswa = ['Mila Rabbani', 'Lorem Ipsum', 'Ipsum'];
 
 // biar hasilnya object, bukan array, dikasih {}, tpi biar {} gadikira function dan mau return, masukin kedalam ()
 // biar hasilnya keren pake console.table
-let jumlahHuruf = mahasiswa.map(nama => ({nama, jmlhHuruf: nama.length}));
-console.table(jumlahHuruf);
+// let jumlahHuruf = mahasiswa.map(nama => ({nama, jmlhHuruf: nama.length}));
+// console.table(jumlahHuruf);
+
+
+// ---------------/------------------/-----------------------/------------------/----------
+
+// Konsep this pada Arrow Function
+// sebenarnya arrow function gabisa pake konsep this
+
+// Constructor Function
+// const Mahasiswa = function () {
+//   this.nama = 'Mila';
+//   this.umur = 33;
+//   this.sayHello = function () {
+//     console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`);
+//   }
+// }
+// const mila = new Mahasiswa();
+
+// Arrow Function, 
+// gabisa buat constructor function pake arrow, tapi klo method didalamnya bisa pake arrow
+// const Mahasiswa = function () {
+//   this.nama = 'Mila';
+//   this.umur = 33;
+//   this.sayHello = () => {
+//     console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`);
+//   }
+// }   //harus di instansiasi
+// const mila = new Mahasiswa();
+
+
+// Object Literal
+// const mhs1 = {
+//   nama : 'Mila',
+//   umur : 33,
+//   sayHello: () => {
+//     console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`);
+//     // console.log(this);
+//   } //gaperlu instansiasi
+// }
+
+
+// contoh lain
+// const Mahasiswa = function () {
+//   this.nama = 'Mila';
+//   this.umur = 33;
+//   this.sayHello = function () {   //function ekspression, masukin kedalam variabel
+//     console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`);
+//   }
+
+//   // setInterval(function() {      //function declaration      will error karna this nya
+//   setInterval(() => { 
+//     console.log(this.umur++);
+//   }, 500);      //setiap 500ms , jalankan function
+// }
+// const mila = new Mahasiswa();
+
+
+// ________________///_____________________//_________________//_________________________//
+
+// const box = document.querySelector('.box');
+// box.addEventListener('click', function() {
+//   this.classList.toggle('size');
+//   setTimeout(() => {      //pake arrow biar this nya bukan window tapi box, karena hoisting
+//     this.classList.toggle('caption');
+//   }, 600);    //setelah 600ms, functionnya jalan
+// })
+
+const box = document.querySelector('.box');
+box.addEventListener('click', function() {
+  let satu = 'size';
+  let dua = 'caption';
+
+  if (this.classList.contains(satu)){
+    [satu, dua] = [dua, satu];      //isinya tukeran, biar efek animasinya reverse
+  }
+
+  this.classList.toggle(satu);    //biar dy ngambil variabel didalam satu, nulisnya jgn 'satu'
+  setTimeout(() => {      //pake arrow biar this nya bukan window tapi box, karena hoisting
+    this.classList.toggle(dua);
+  }, 600);    //setelah 600ms, functionnya jalan
+})
