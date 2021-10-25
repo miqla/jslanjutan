e62310c6a1f2bb31128ff38c99e0066da3f15dd6
miqla <miqdad.shaldin@gmail.com>
2021-10-25 16:45:39 +0700

    Async dan Await. buat promise sederhana, nyoba try and catch utk error handling.

---
+++ asyncawait.js
@@ -0,0 +1,61 @@
+// bikin promise
+// const coba = new Promise(resolve => resolve('selesai'));    // ini syncoonous
+// console.log(coba);
+
+
+// biar dy nunggu dulu
+// const coba = new Promise(resolve => {
+//   setTimeout(() => {
+//     resolve('selesai')
+//   }, 2000);
+// });
+// coba.then(()=>console.log(coba));
+
+
+// function cobaPromise() {
+//   return new Promise(resolve => {
+//     setTimeout(() => {
+//       resolve('selesai')
+//     }, 2000);
+//   });
+// }
+
+// // const coba = cobaPromise();
+// // coba.then(()=>console.log(coba));
+
+// async function cobaAsync() {
+//   const coba = await cobaPromise();
+//   console.log(coba);
+// }
+// cobaAsync();
+
+
+
+
+function cobaPromise() {
+  return new Promise((resolve, reject) => {
+    const waktu = 5000;       //3000ms
+    if(waktu < 5000) {
+      setTimeout(() => {
+        resolve('selesai')
+      }, waktu);
+    } else {
+      reject('kelamaan!');
+    }
+  });
+}
+// const coba = cobaPromise();
+// coba
+//   .then(()=>console.log(coba))
+//   .catch(()=>console.log(coba));
+
+async function cobaAsync() {
+  try {
+    const coba = await cobaPromise();
+    console.log(coba);
+  } catch(err) {
+    // console.log(err);
+    console.error(err);   //biar warnanya merah
+  }
+}
+cobaAsync();
